e0cb0089356c46f076e3039f3cece6d9102c8421
rottylr <mplogin@teampierre.com>
2015-07-12 13:40:41 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -20,5 +20,6 @@
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
+    [self addObstacle];
 }
 @end
